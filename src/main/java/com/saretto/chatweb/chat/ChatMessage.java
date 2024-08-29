1e30a31ecbb21935ff65380974b772d196dd8c30
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-08-28 23:03:43 -0300

    Criando um modelo de mensagem

---
+++ src/main/java/com/saretto/chatweb/chat/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.saretto.chatweb.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, JOIN, LEAVE
+    }
+}
--- src/main/java/com/saretto/chatweb/config/WebSocketConfig.java
+++ src/main/java/com/saretto/chatweb/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.saretto.chatweb;
+package com.saretto.chatweb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
